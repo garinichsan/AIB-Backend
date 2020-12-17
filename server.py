19b8d6f0d9366c72531ad7b74e24ea7c52db1af0
Garin <garin.kra@gmail.com>
2020-12-17 15:10:20 +0700

    fix: parsing json

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 import numpy as np
 from flask import Flask, request, jsonify
 import pickle
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -9,30 +11,32 @@ app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
 
 #contoh data
-data = [{
-    "marriage" : "1",
-    "sex" : "1",
-    "edu" : "1",
-    "limit" : "1",
-    "pay_amt_1" : "1",
-    "pay_amt_2" : "1",
-    "pay_amt_3" : "1",
-    "pay_amt_4" : "1",
-    "pay_amt_5" : "1",
-    "pay_amt_6" : "1",
-    "bil_amt_1" : "1",
-    "bil_amt_2" : "1",
-    "bil_amt_3" : "1",
-    "bil_amt_4" : "1",
-    "bil_amt_5" : "1",
-    "bil_amt_6" : "1",
-    "pay_status_1" : "1",
-    "pay_status_2" : "1", 
-    "pay_status_3" : "1", 
-    "pay_status_4" : "1", 
-    "pay_status_5" : "1", 
-    "pay_status_6" : "1", 
-  }]
+contoh = {
+    "marriage" : 1,
+    "sex" : 1,
+    "edu" : 1,
+    "limit" : 1,
+    "age" : 37,
+    "pay_amt_1" : 2000.0,
+    "pay_amt_2" : 2019.0,
+    "pay_amt_3" : 1200.0,
+    "pay_amt_4" : 1100.0,
+    "pay_amt_5" : 1069.0,
+    "pay_amt_6" : 1000.0,
+    "bil_amt_1" : 46990.0,
+    "bil_amt_2" : 48233.0,
+    "bil_amt_3" : 49291.0,
+    "bil_amt_4" : 28314.0,
+    "bil_amt_5" : 28959.0,
+    "bil_amt_6" : 29547.0,
+    "pay_status_1" : 0,
+    "pay_status_2" : 0, 
+    "pay_status_3" : 0, 
+    "pay_status_4" : 0, 
+    "pay_status_5" : 0, 
+    "pay_status_6" : 0, 
+    "sex_marriage" : 2
+  }
 
 @app.route('/', methods=['GET'])
 def home():
@@ -48,15 +52,22 @@ def predict():
     # Get the data from the POST request.
     data = request.get_json(force=True)
 
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([[np.array(data['LIMIT_BAL', 'SEX', 'EDUCATION', 'MARRIAGE', 'AGE', 'PAY_1',
-       'PAY_2', 'PAY_3', 'PAY_4', 'PAY_5', 'PAY_6', 'BILL_AMT1', 'BILL_AMT2',
-       'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6', 'PAY_AMT1',
-       'PAY_AMT2', 'PAY_AMT3', 'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6'])]])
+    # # Make prediction using model loaded from disk as per the data.
+    df = pd.DataFrame(data, index=[0])
+    test = df[["limit","sex","edu","marriage","age",
+        "pay_status_1","pay_status_2","pay_status_3","pay_status_4","pay_status_5","pay_status_6",
+        "bil_amt_1","bil_amt_2","bil_amt_3","bil_amt_4","bil_amt_5","bil_amt_6",
+        "pay_amt_1","pay_amt_2","pay_amt_3","pay_amt_4","pay_amt_5","pay_amt_6",
+        "sex_marriage"]]
+    prediction = model.predict(test)
 
     # Take the first value of prediction
     output = prediction[0]
-    return jsonify(output)
+
+    # terminal output debug
+    # app.logger.info( output )
+
+    return jsonify(str(output))
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
